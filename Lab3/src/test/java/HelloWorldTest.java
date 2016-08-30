efcc848702d70f73f01ef22b3add07d8d6ce3ea6
Aldor <aldaer@yandex.ru>
2016-08-30 17:27:16 +0300

    More Spring options

--- Lab3/src/test/java/HelloWorldTest.java
+++ Lab3/src/test/java/HelloWorldTest.java
@@ -1,7 +1,9 @@
-import static org.junit.Assert.* ;
+import static org.junit.Assert.*;
+
 import org.junit.*;
 
 import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 import lab.model.UsualPerson;
@@ -10,44 +12,43 @@ import lab.model.Country;
 
 public class HelloWorldTest {
 
-	protected static final String APPLICATION_CONTEXT_XML_FILE_NAME = "src/main/resources/application-context.xml";
+    protected static final String APPLICATION_CONTEXT_XML_FILE_NAME = "application-context.xml";
 
-	private UsualPerson expectedPerson;
+    private UsualPerson expectedPerson;
 
-	private AbstractApplicationContext context;
+    private AbstractApplicationContext context;
 
-	@Before
-	public void setUp() throws Exception {
-		context = new FileSystemXmlApplicationContext(
-				new String[] { APPLICATION_CONTEXT_XML_FILE_NAME });
-		expectedPerson = getExpectedPerson();
-	}
+    @Before
+    public void setUp() throws Exception {
+        context = new ClassPathXmlApplicationContext(APPLICATION_CONTEXT_XML_FILE_NAME);
+        expectedPerson = getExpectedPerson();
+    }
 
-	@Test
-	public void testInitPerson() {
-		UsualPerson person = (UsualPerson) context.getBean("person", Person.class);
-		assertEquals(expectedPerson, person);
-		System.out.println(person);
-	}
+    @Test
+    public void testInitPerson() {
+        UsualPerson person = (UsualPerson) context.getBean("person", Person.class);  // "person" is optional, since only one object implements Person interface
+        assertEquals(expectedPerson, person);
+        System.out.println(person);
+    }
 
-	private UsualPerson getExpectedPerson() {
-		UsualPerson person = new UsualPerson();
-		person.setAge(35);
-		person.setName("John Smith");
+    private UsualPerson getExpectedPerson() {
+        UsualPerson person = new UsualPerson();
+        person.setAge(35);
+        person.setName("John Smith");
 
-		Country country = new Country();
-		country.setId(1);
-		country.setName("Russia");
-		country.setCodeName("RU");
+        Country country = new Country();
+        country.setId(1);
+        country.setName("Russia");
+        country.setCodeName("RU");
 
-		person.setCountry(country);
+        person.setCountry(country);
 
-		return person;
-	}
-	
-	@After
-	public void tearDown() throws Exception{
-		if (context != null)
-			context.close();
-	}
+        return person;
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        if (context != null)
+            context.close();
+    }
 }
