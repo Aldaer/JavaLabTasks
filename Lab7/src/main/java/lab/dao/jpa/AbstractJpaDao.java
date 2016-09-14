5c6a6d225f0ecd550a3b9e2fc492ff9096c0b920
Artem Lodygin <aldaer@yandex.ru>
2016-09-14 17:36:08 +0300

    Lab8

--- Lab7/src/main/java/lab/dao/jpa/AbstractJpaDao.java
+++ Lab7/src/main/java/lab/dao/jpa/AbstractJpaDao.java
@@ -4,16 +4,6 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 
 public class AbstractJpaDao {
-
-	protected EntityManagerFactory emf;
-
-	public AbstractJpaDao() {
-		super();
-	}
-
 	@PersistenceUnit
-	public void setEntityManagerFactory(EntityManagerFactory emf) {
-		this.emf = emf;
-	}
-
+	protected EntityManagerFactory emf;
 }
